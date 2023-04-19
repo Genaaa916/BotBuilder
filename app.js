19ffbd9cdfe189bbc73c33e6acf8c444eab5fb80
Gena <genci.behxheti@gmail.com>
2023-04-19 14:49:13 +0300

    -

--- app.js
+++ app.js
@@ -15,66 +15,66 @@ let choiceSent = false
 
 
 const sendUserChoice = () => {
-    fetch("http://localhost:3000/choices", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            userChoice
-        })
-        }).then((res) => res.json())
+  fetch("http://localhost:3000/choices", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      userChoice
+    })
+  }).then((res) => res.json())
 }
 
 const copyToClip = (regex, string, parent) => {
-  
-        const copyString = string.match(regex)
-        const copyButton = document.createElement("button")
-        copyButton.textContent = "Copy to clipboard";
-        parent.appendChild(copyButton)
-        copyButton.scrollTop = parent.scrollHeight
-        const classesToAdd = ["w-1/2", "mx-auto", "transition", "duration-300", "ease-in-out", "rounded", "p-2", "hover:bg-blue-400", "hover:text-white", "mx-5", "my-2"]
-        classesToAdd.forEach(cls => {
-          copyButton.classList.add(cls)
-        })
-        copyButton.addEventListener("click", () => {
-          navigator.clipboard.writeText(copyString)
-          const copiedText = document.createElement("p")
-          copiedText.innerHTML = "Copied! Now just paste into the Bot Builder!"
-          chatlog.appendChild(copiedText)
-        })
-        return copyString
-      }
+
+  const copyString = string.match(regex)
+  const copyButton = document.createElement("button")
+  copyButton.textContent = "Copy to clipboard";
+  parent.appendChild(copyButton)
+  copyButton.scrollTop = parent.scrollHeight
+  const classesToAdd = ["w-1/2", "mx-auto", "transition", "duration-300", "ease-in-out", "rounded", "p-2", "hover:bg-blue-400", "hover:text-white", "mx-5", "my-2"]
+  classesToAdd.forEach(cls => {
+    copyButton.classList.add(cls)
+  })
+  copyButton.addEventListener("click", () => {
+    navigator.clipboard.writeText(copyString)
+    const copiedText = document.createElement("p")
+    copiedText.innerHTML = "Copied! Now just paste into the Bot Builder!"
+    chatlog.appendChild(copiedText)
+  })
+  return copyString
+}
 
 
 const allButtons = [botBtn, useCaseBtn, toneBtn]
-allButtons.forEach(group => { 
-group.forEach(button => {
-  button.addEventListener('click', () => {
-    button.classList.add('bg-black');
-    button.classList.add('text-white');
-    choiceSent = false
+allButtons.forEach(group => {
+  group.forEach(button => {
+    button.addEventListener('click', () => {
+      button.classList.add('bg-black');
+      button.classList.add('text-white');
+      choiceSent = false
 
-    group.forEach(otherButton => {
-      if (otherButton !== button) {
-        otherButton.classList.remove('bg-black');
-        otherButton.classList.remove('text-white');
-      }
+      group.forEach(otherButton => {
+        if (otherButton !== button) {
+          otherButton.classList.remove('bg-black');
+          otherButton.classList.remove('text-white');
+        }
+      });
     });
   });
-});
 
 })
 
 const rememberUserChoice = (key, button) => {
-    button.addEventListener("click", (e) => {
+  button.addEventListener("click", (e) => {
     e.preventDefault();
     //for running in node
     userChoice[key] = button.innerText;
     //for running in the browser w/ sessiondata
     window.sessionStorage.setItem(key, button.innerText);
-    }
-)}
+  })
+}
 
 allButtons.forEach(group => {
   group.forEach(element => {
@@ -91,48 +91,51 @@ submit.addEventListener("click", (e) => {
   userChoice["end"] = end.value
 
 
-if(!choiceSent){
-  sendUserChoice()
-  choiceSent = true
-}
-e.preventDefault();
-const msg = message.value;
-const newMsg = {"role": "user", "content": `${msg}`}
-/* msg_array.push(newMsg) */
-message.value = "";
-const msgElement = document.createElement("div");
-msgElement.classList.add(["message", "message-sent"]);
-msgElement.innerHTML = `<div class="msg-text">${msg}</div>`;
+  if (!choiceSent) {
+    sendUserChoice()
+    choiceSent = true
+  }
+  e.preventDefault();
+  const msg = message.value;
+  const newMsg = {
+    "role": "user",
+    "content": `${msg}`
+  }
+  /* msg_array.push(newMsg) */
+  message.value = "";
+  const msgElement = document.createElement("div");
+  msgElement.classList.add(["message", "message-sent"]);
+  msgElement.innerHTML = `<div class="msg-text">${msg}</div>`;
 
   chatlog.appendChild(msgElement);
   chatlog.scrollTop = chatlog.scrollHeight;
-    fetch("http://localhost:3000/chat", {
+  fetch("http://localhost:3000/chat", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-          message: newMsg
+        message: newMsg
       })
     })
-      .then((res) => res.json())
-      .then((data) => {
-      
+    .then((res) => res.json())
+    .then((data) => {
+
       const reply = data.completion.content
-/*       let newMsg = {"role": "assistant", "content": `${reply}`}
-      msg_array.push(newMsg) */
+      /*       let newMsg = {"role": "assistant", "content": `${reply}`}
+            msg_array.push(newMsg) */
       const msgElement = document.createElement("div");
       msgElement.classList.add(["message", "message-received"]);
-      if (!reply.match(regex)){
+      if (!reply.match(regex)) {
         msgElement.innerHTML = `<div class="msg-text m-5 md:mx-8 font-semibold">${reply}</div>`
-      } 
+      }
       chatlog.appendChild(msgElement);
       chatlog.scrollTop = chatlog.scrollHeight;
       window.scrollTo(0, document.body.scrollHeight)
-      if (reply.match(regex)){
+      if (reply.match(regex)) {
         const remove = copyToClip(regex, reply, chatlog)
-        msgElement.innerHTML = `<div class="msg-text m-5 md:mx-8 font-semibold">${reply.replace(remove, "").replace(/'{2,3}/g, "")}</div>` 
+        msgElement.innerHTML = `<div class="msg-text m-5 md:mx-8 font-semibold">${reply.replace(remove, "").replace(/'{2,3}/g, "")}</div>`
       }
-      });
-  
+    });
+
 });
--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-import { Configuration, OpenAIApi } from "openai";
+import {
+    Configuration,
+    OpenAIApi
+} from "openai";
 import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
@@ -8,9 +11,9 @@ import fs from 'fs';
 
 dotenv.config();
 let choiceData = []
-const config = new Configuration ({
+const config = new Configuration({
     organization: "",
-    apiKey : process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 })
 const openai = new OpenAIApi(config);
 
@@ -25,7 +28,9 @@ app.use(bodyParser.json());
 app.post('/choices', async (req, res) => {
     choiceData = []
     const userChoice = req.body.userChoice;
-    res.json({userChoice: userChoice})
+    res.json({
+        userChoice: userChoice
+    })
     choiceData.push(userChoice)
     console.log(userChoice)
 })
@@ -33,45 +38,46 @@ app.post('/choices', async (req, res) => {
 
 app.post('/chat', async (req, res) => {
     const prompt = req.body.message
-    const instructions = {botType: choiceData[0].bot.toLowerCase(), 
-        industry: choiceData[0].industry.toLowerCase(), 
-        useCase: choiceData[0].useCase.toLowerCase(), 
-        tone:choiceData[0].tone.toLowerCase(), 
+    const instructions = {
+        botType: choiceData[0].bot.toLowerCase(),
+        industry: choiceData[0].industry.toLowerCase(),
+        useCase: choiceData[0].useCase.toLowerCase(),
+        tone: choiceData[0].tone.toLowerCase(),
         additional: choiceData[0].additional.toLowerCase(),
-        start:choiceData[0].start.toLowerCase(),
-        end:choiceData[0].end.toLowerCase()
+        start: choiceData[0].start.toLowerCase(),
+        end: choiceData[0].end.toLowerCase()
     }
-    
-const getExampleJSON = async filePath => {
-  try {
-    const data = await fs.promises.readFile(filePath, 'utf8')
-    return JSON.stringify(data).replace("inpage", instructions.botType).replaceAll("\\", "")
-  }
-  catch(err) {
-    console.log(err)
-  }
-}
+
+    const getExampleJSON = async filePath => {
+        try {
+            const data = await fs.promises.readFile(filePath, 'utf8')
+            return JSON.stringify(data).replace("inpage", instructions.botType).replaceAll("\\", "")
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
 
-instructions.example = await getExampleJSON('example.json')
+    instructions.example = await getExampleJSON('example.json')
 
-    
+
     //systemMsg is the instructions for the chatbot, it should vary depending on user choices on the site.
-let systemMsg = `Pretend you're a program that generates JSON templates for chatbots to follow. The user wants to implement a ${instructions.useCase} chatbot on their website. The conversation of the chatbot should be in a ${instructions.tone} tone. The conversation should start from:${instructions.start} and end in:${instructions.end}  Their company is in the ${instructions.industry} industry. The user may have some discussion points in mind, which will be listed at the end of the prompt. Your output should be valid JSON, specifically it should mirror the structure of this JSON file ${instructions.example}
+    let systemMsg = `Pretend you're a program that generates JSON templates for chatbots to follow. The user wants to implement a ${instructions.useCase} chatbot on their website. The conversation of the chatbot should be in a ${instructions.tone} tone. The conversation should start from:${instructions.start} and end in:${instructions.end}  Their company is in the ${instructions.industry} industry. The user may have some discussion points in mind, which will be listed at the end of the prompt. Your output should be valid JSON, specifically it should mirror the structure of this JSON file ${instructions.example}
 Please note that node types dictate what the purpose of a node is and is mapped like this: {"multiplechoice answers":14,"dropdown menu answers":15,"singlechoice answers":11,"openfield answers":5}. Note that only singlechoice answers can branch out to multiple alternative questions. Also make sure you generate at least 8 nodes. Make sure the JSON you generate is minified. Generate the JSON file now.`
-console.log(systemMsg)
-console.log(req.body)
-        const completion = await openai.createChatCompletion({
-        model : "gpt-3.5-turbo",
-        messages : [
-            {role: "system", content: systemMsg}, prompt
-        ]
+    console.log(systemMsg)
+    console.log(req.body)
+    const completion = await openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{
+            role: "system",
+            content: systemMsg
+        }, prompt]
     })
-    res.json({completion: completion.data.choices[0].message})
+    res.json({
+        completion: completion.data.choices[0].message
+    })
 })
 
 app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
-
-
